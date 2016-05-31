e90c1b74ffad93f2ae659468f5fd5810538795bc
Amer Hamdan <amashamdan@gmail.com>
2016-05-31 21:28:05 +0300

    Feat: Added RecipeDetails component.
    This new component displays the details of the a recipe.
    It includes a header, ingredients list, a delete and an edit buttons.
    The page should load with the component hidden. This is the next step.

--- scripts/components.js
+++ scripts/components.js
@@ -21,16 +21,41 @@ var Box = React.createClass({
 })
 
 var RecipeList = React.createClass({
+	handleClick: function(recipe) {
+		console.log(recipe);
+	},
 	render: function() {
+		/* passing this.handleClick directly to onClick props results in error... seems like scoping error. */
+		var handler = this.handleClick;
+		//var data = this.props.recipes; 
 		var recipeElements = this.props.recipes.map(function(recipe){
+			/* Bind returns a function with a body same as the function it's bound to. this is resolved to the first argument.
+			So when you call it with onfilechange.bind(null, playsound), it creates and returns a new function, always receiving playsound as first argument and using global context (Because null is used as context), just like all regular functions use global context */
 			return (
-				<li key={recipe.name}>{recipe.name}</li>				
+				<li key={recipe.name}><span onClick={handler.bind(null, recipe)}>{recipe.name}</span><RecipeDetails ingredients={recipe.ingredients} /></li>
+				
 			);
 		});
 		return (
 			<ul className="recipe-list">
 				{recipeElements}
 			</ul>
+		);
+	}
+})
+
+var RecipeDetails = React.createClass({
+	render: function() {
+		var ingredients = this.props.ingredients.map(function(ingredient) {
+			return (<div key={ingredient} className="ingredient">{ingredient}</div>);
+		})
+		return (
+			<div className="recipe-details">
+				<h4 className="ingredients-header">Ingredients</h4>
+				{ingredients}
+				<button className="edit">Edit</button>
+				<button className="delete">Delete</button>
+			</div>
 		);
 	}
 })
